6429175819e3f8750dcc3de6680a471aa29c0258
Jakub Vrana <jvrana@monetplus.cz>
2018-11-05 10:38:53 +0100

    Repeating Test with JUnit 5

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -31,4 +33,11 @@ class PersonTest implements ModelTests {
                 () -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
     }
 
+
+    @RepeatedTest(value = 10, name="{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My repeated test")
+    @Test
+    void myRepeatedTest() {
+        // todo - impl
+    }
 }
