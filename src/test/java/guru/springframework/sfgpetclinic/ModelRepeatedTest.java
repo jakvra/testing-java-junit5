ae5225eb4558a9b49bf2422a9c45bf6f83782852
Jakub Vrana <jvrana@monetplus.cz>
2018-11-14 23:11:31 +0100

    JUnit Parameterized test - ValueSource

---
+++ src/test/java/guru/springframework/sfgpetclinic/ModelRepeatedTest.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInfo;
+
+@Tag("model")
+public interface ModelRepeatedTest {
+
+    @BeforeEach
+    default void beforeEachConsoleOutputer(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println("Running Test - " + testInfo.getDisplayName() + " - " + repetitionInfo.getCurrentRepetition() +
+                " | " + repetitionInfo.getTotalRepetitions());
+    }
+
+}
+
--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinic.model;
 import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -35,4 +37,10 @@ class OwnerTest implements ModelTests {
         assertThat(owner.getCity(), is("Key West")); // hamrcrest library
     }
 
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Spring", "Framework", "Guru"})
+    void testValueSource(String val) {
+        System.out.println(val);
+    }
 }
