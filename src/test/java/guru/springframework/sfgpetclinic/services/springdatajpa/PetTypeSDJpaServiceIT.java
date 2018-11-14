5404a302a850332e796160b054d8f0b362e93889
Jakub Vrana <jvrana@monetplus.cz>
2018-11-14 23:54:51 +0100

    JUnit Extensions

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import guru.springframework.junitextensions.TimingExtensions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *  INTEGRATION TEST - maven recognized by IT suffix
  */
+@ExtendWith(TimingExtensions.class)
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
