1b08e6ac0d9a3d5d90f1459196abd9d9927ece4d
Jakub Vrana <jvrana@monetplus.cz>
2018-11-14 23:30:31 +0100

    JUnit Parameterized test - CSV File Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -55,14 +56,21 @@ class OwnerTest implements ModelTests {
         System.out.println(ownerType);
     }
 
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @CsvSource({
             "FL, 1, 1",
             "OH, 2, 2",
             "MI, 3, 3",
     })
-    @DisplayName("CSV Input Test")
-    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    @DisplayName("CSV From Filet Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 }
